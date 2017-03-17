070d131f19c48b16bef2acd61098ff39e9bc68ba
Michael <michaelbrown18@gmail.com>
2017-03-17 07:14:45 -0400

    Completed desktop and mobile header styles.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -17,9 +17,18 @@ watch('./app/index.html', function() {
 watch('./app/assets/styles/**/*.css', function() {
   gulp.start('cssInject');
 });
+
+watch('./app/assets/scripts/**/*.js', function() {
+  gulp.start('scriptsRefresh');
+});
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
